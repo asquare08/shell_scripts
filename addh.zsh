4b71b2c3d1f6f3e6ff4eb32839694e98e2b6ce38
Atul Agarwal <atul.agarwal@iitj.ac.in>
2021-09-13 18:02:24 +0530

    output hex add and sub as dec

--- addh.zsh
+++ addh.zsh
@@ -3,4 +3,4 @@ num1="$(echo $1 | sed -e 's/^0x//i' -e 's/^-0x//i')"
 num1=${num1:u}
 num2="$(echo $2 | sed -e 's/^0x//i' -e 's/^-0x//i')"
 num2=${num2:u}
-echo "obase=16;ibase=16;$num1+$num2"|bc
+echo "obase=10;ibase=16;$num1+$num2"|bc
